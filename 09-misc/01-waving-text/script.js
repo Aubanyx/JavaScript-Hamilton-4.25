440744c0d93049c6397a04d4b420432f8026c09c
Aubanyx <aubanlabie@gmail.com>
2020-11-09 08:56:15 +0100

    modif

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -15,25 +15,52 @@
 
     let target = document.getElementById("target");
     let phrase = document.getElementById("target").innerHTML;
+    let lettre = [];
+    let cpt = 0;
+
     console.log(phrase.length);
 
     for (let i = 0; i <= phrase.length; i++) {
         console.log(phrase[i]);
+        lettre.push(phrase[i]);
     }
-
-
-    let lettre = 0;
     console.log(lettre);
 
-    function changeFontSize() {
-        phrase[lettre].style.fontSize = "25px";
-        console.log("test");
-        console.log(phrase[lettre]);
-        lettre++;
-        console.log(lettre);
+    lettre[0].fontsize(8);
+    lettre[1].style.fontsize = "25px";
 
-    }
+    // function changeFontSize() {
+    //     console.log(cpt);
+    //     console.log(lettre[cpt]);
+    //     if (lettre[cpt] <= 74) {
+    //         lettre[cpt].fontsize(8);
+    //     }
+    //     else {
+    //         cpt++;
+    //     }
+    // }
+
+    // phrase.forEach(item => console.log(item));
 
         // let time = setInterval(changeFontSize, 1000);
+
+
+
+    // const target = document.getElementById("target");
+    // const text = [...target.innerText].map(x => `<span>${x}</span>`).join("");
+    // let waver = 0;
+    //
+    // target.innerHTML = text;
+    //
+    // setInterval(wave, 100, target);
+    //
+    // function wave(target){
+    //
+    //     for (let i=target.childNodes.length-1 ; i>0 ; i--) {
+    //         target.childNodes[i].style.fontSize = target.childNodes[i-1].style.fontSize;
+    //     }
+    //     waver+=0.3;
+    //     target.childNodes[0].style.fontSize = `${2 + Math.sin(waver)}rem`;
+    // }
 
 })();
--- 09-misc/06-guess-number/script.js
+++ 09-misc/06-guess-number/script.js
@@ -12,5 +12,58 @@
 (function() {
 
     // your code here
+    let randomNumber = Math.floor(Math.random() * 100);
+    console.log(randomNumber);
+
+    function guessTheNumber() {
+        let i = 1;
+        let askingTheNumber = prompt("Guess the number bettween 0 and 100!");
+
+        if (askingTheNumber < randomNumber) {
+            let newTry = prompt("Wrong guess, it's more, try again. This was try number " + i);
+
+            while (true) {
+                i++;
+                if (newTry < randomNumber) {
+                    let stillWrong = prompt("Still wrong, it's more, try again. This was try number " + i);
+                }
+
+                else if (newTry > randomNumber) {
+                    let stillWrong = prompt("Still wrong, it's less, try again. This was try number " + i);
+                }
+
+                else if (newTry == randomNumber) {
+                    alert ("Great, the number was ideed " + randomNumber + " You needed " + i + " tries");
+                    break;
+                }
+            }
+        }
+
+
+        else if (askingTheNumber > randomNumber) {
+            let newTry = prompt("Wrong guess, it's less, try again. This was try number " + i);
+
+            while (true) {
+                i++;
+                if (newTry < randomNumber) {
+                    let stillWrong = prompt("Still wrong, it's more, try again. This was try number " + i);
+                }
+
+                else if (newTry > randomNumber) {
+                    let stillWrong = prompt("Still wrong, it's less, try again. This was try number " + i);
+                }
+
+                else if (newTry == randomNumber) {
+                    alert ("Great, the number was ideed " + randomNumber + " You needed " + i + " tries");
+                    break;
+                }
+            }
+        }
+        else {
+            alert ("Waow, first try, that's amazing!!!");
+        }
+    }
+
+    guessTheNumber();
 
 })();
